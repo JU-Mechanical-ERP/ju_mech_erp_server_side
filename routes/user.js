0facbe5ae5ad2ac939cbeaa006caa1e67209f9cf
MauryaSamanta <mauryasamanta1508@gmail.com>
2025-03-27 19:39:24 +0530

    Requests feature complete

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import upload from '../helpers/multer.js';
-import { login, signup, updateUser } from '../controllers/user.js';
+import { createreq, getUserRequests, login, signup, updateUser } from '../controllers/user.js';
 
 const router = express.Router();
 router.post('/login', login);
 router.post('/signup', signup);
 router.post('/updatedetails', updateUser);
+router.post('/createreq', createreq);
+router.post('/getreqs', getUserRequests);
 export default router;
