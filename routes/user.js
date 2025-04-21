4c1fffc8d91d731c4c30c6461628e859491d053a
RSWDevian <pingking29705@gmail.com>
2025-04-21 16:53:08 +0530

    Added the primary fetch user controller

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,20 @@
-import express from 'express';
-import upload from '../helpers/multer.js';
-import { createreq, getUserRequests, login, signup, updateUser } from '../controllers/user.js';
+import express from "express";
+import upload from "../helpers/multer.js";
+import {
+  createreq,
+  getUserRequests,
+  login,
+  signup,
+  getPrimaryUserDetails,
+  updateUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
 //? backend routes
-router.post('/login', login); // for user login
-router.post('/signup', signup); // for user signup
-router.post('/updatedetails', updateUser); // for updating details
-router.post('/createreq', createreq); 
-router.post('/getreqs', getUserRequests);
+router.post("/login", login); // for user login
+router.post("/signup", signup); // for user signup
+router.get("/creds-primary", getPrimaryUserDetails);
+router.post("/updatedetails", updateUser); // for updating details
+router.post("/createreq", createreq);
+router.post("/getreqs", getUserRequests);
 export default router;
